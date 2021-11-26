ac169720e8866de1bf84195b327890bae37b48c0
AleksandrSotnikov <sotnikovalex143@gmail.com>
2021-11-26 15:17:32 +0300

    #15 ReformatCode, add Insets

--- ui/home/src/main/kotlin/com/ronasit/home/BottomNavigationBar.kt
+++ ui/home/src/main/kotlin/com/ronasit/home/BottomNavigationBar.kt
@@ -1,14 +1,17 @@
 package com.ronasit.home
 
+import androidx.compose.foundation.background
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.google.accompanist.insets.navigationBarsWithImePadding
 import com.ronasit.core.ui.theme.RickAndMortyTheme
 import com.ronasit.navigation.NavigationItem
 
@@ -23,7 +26,10 @@ internal fun BottomNavigationBar(navController: NavController) {
     BottomNavigation(
         backgroundColor = RickAndMortyTheme.colors.blackCard,
         contentColor = RickAndMortyTheme.colors.white,
-        elevation = 1.dp
+        elevation = 1.dp,
+        modifier = Modifier
+            .background(RickAndMortyTheme.colors.blackCard)
+            .navigationBarsWithImePadding()
     ) {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
--- ui/location/list/src/main/kotlin/com/ronasit/episode/list/LocationListScreen.kt
+++ ui/location/list/src/main/kotlin/com/ronasit/episode/list/LocationListScreen.kt
@@ -3,8 +3,6 @@ package com.ronasit.episode.list
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
